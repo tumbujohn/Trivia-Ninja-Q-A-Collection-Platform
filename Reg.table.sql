e0b7da21623316dbf4789a362ff518a87b096c76
Ndi NLomo Raoul <110162328+NdiRaoul@users.noreply.github.com>
2022-08-23 11:17:59 +0100

    Add files via upload

---
+++ Reg.table.sql
@@ -0,0 +1,61 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 26, 2022 at 03:25 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+01:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `Trivia_Ninja_Q_A_Db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `User_Info`
+--
+
+CREATE TABLE `User_Info` (
+  `id` int(11) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `reg_date` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `User_Info`
+--
+ALTER TABLE `User_Info`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `User_Info`
+--
+ALTER TABLE `User_Info`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ Register.php
@@ -0,0 +1,127 @@
+<?php
+# Include connection
+require_once "./Connection.php";
+
+# Define variables and initialize with empty values
+$username_err = $email_err = $password_err = "";
+$username = $email = $password = "";
+
+# Processing form data when form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  # Validate username
+  if (empty(trim($_POST["username"]))) {
+    $username_err = "Please enter a username.";
+  } else {
+    $username = trim($_POST["username"]);
+    if (!ctype_alnum(str_replace(array("@", "-", "_"), "", $username))) {
+      $username_err = "Username can only contain letters, numbers and symbols like '@', '_', or '-'.";
+    } else {
+      # Prepare a select statement
+      $sql = "SELECT id FROM users WHERE username = ?";
+
+      if ($stmt = mysqli_prepare($link, $sql)) {
+        # Bind variables to the statement as parameters
+        mysqli_stmt_bind_param($stmt, "s", $param_username);
+
+        # Set parameters
+        $param_username = $username;
+
+        # Execute the prepared statement 
+        if (mysqli_stmt_execute($stmt)) {
+          # Store result
+          mysqli_stmt_store_result($stmt);
+
+          # Check if username is already registered
+          if (mysqli_stmt_num_rows($stmt) == 1) {
+            $username_err = "This username is already registered.";
+          }
+        } else {
+          echo "<script>" . "alert('Oops! Something went wrong. Please try again later.')" . "</script>";
+        }
+
+        # Close statement 
+        mysqli_stmt_close($stmt);
+      }
+    }
+  }
+
+  # Validate email 
+  if (empty(trim($_POST["email"]))) {
+    $email_err = "Please enter an email address";
+  } else {
+    $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $email_err = "Please enter a valid email address.";
+    } else {
+      # Prepare a select statement
+      $sql = "SELECT id FROM users WHERE email = ?";
+
+      if ($stmt = mysqli_prepare($link, $sql)) {
+        # Bind variables to the statement as parameters
+        mysqli_stmt_bind_param($stmt, "s", $param_email);
+
+        # Set parameters
+        $param_email = $email;
+
+        # Execute the prepared statement 
+        if (mysqli_stmt_execute($stmt)) {
+          # Store result
+          mysqli_stmt_store_result($stmt);
+
+          # Check if email is already registered
+          if (mysqli_stmt_num_rows($stmt) == 1) {
+            $email_err = "This email is already registered.";
+          }
+        } else {
+          echo "<script>" . "alert('Oops! Something went wrong. Please try again later.');" . "</script>";
+        }
+
+        # Close statement
+        mysqli_stmt_close($stmt);
+      }
+    }
+  }
+
+  # Validate password
+  if (empty(trim($_POST["password"]))) {
+    $password_err = "Please enter a password.";
+  } else {
+    $password = trim($_POST["password"]);
+    if (strlen($password) < 8) {
+      $password_err = "Password must contain at least 8 or more characters.";
+    }
+  }
+
+  # Check input errors before inserting data into database
+  if (empty($username_err) && empty($email_err) && empty($password_err)) {
+    # Prepare an insert statement
+    $sql = "INSERT INTO users(username, email, password) VALUES (?, ?, ?)";
+
+    if ($stmt = mysqli_prepare($link, $sql)) {
+      # Bind varibales to the prepared statement as parameters
+      mysqli_stmt_bind_param($stmt, "sss", $param_username, $param_email, $param_password);
+
+      # Set parameters
+      $param_username = $username;
+      $param_email = $email;
+      $param_password = password_hash($password, PASSWORD_DEFAULT);
+
+      # Execute the prepared statement
+      if (mysqli_stmt_execute($stmt)) {
+        echo "<script>" . "alert('Registeration completed successfully. Login to continue.');" . "</script>";
+        echo "<script>" . "window.location.href='./login.php';" . "</script>";
+        exit;
+      } else {
+        echo "<script>" . "alert('Oops! Something went wrong. Please try again later.');" . "</script>";
+      }
+
+      # Close statement
+      mysqli_stmt_close($stmt);
+    }
+  }
+
+  # Close connection
+  mysqli_close($link);
+}
+?>
+
