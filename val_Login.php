c6e20c710a2f601ed74759a3dc34a5e9b9e1ca94
Nadine Abla <ablanadine845@gmail.com>
2022-08-24 16:09:17 +0100

    created login

---
+++ val_Login.php
@@ -0,0 +1,89 @@
+<?php
+# Initialize session
+session_start();
+
+# Check if user is already logged in, If yes then redirect him to index page
+if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] == TRUE) {
+  echo "<script>" . "window.location.href='./'" . "</script>";
+  exit;
+}
+
+# Include connection
+require_once ".lib/helpers/connection.db.php";
+
+# Define variables and initialize with empty values
+$user_login_err = $user_password_err = $login_err = "";
+$user_login = $user_password = "";
+
+# Processing form data when form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty(trim($_POST["user_login"]))) {
+    $user_login_err = "Please enter your username or an email id.";
+  } else {
+    $user_login = trim($_POST["user_login"]);
+  }
+
+  if (empty(trim($_POST["user_password"]))) {
+    $user_password_err = "Please enter your password.";
+  } else {
+    $user_password = trim($_POST["user_password"]);
+  }
+
+  # Validate credentials 
+  if (empty($user_login_err) && empty($user_password_err)) {
+    # Prepare a select statement
+    $sql = "SELECT id, username, password FROM users WHERE username = ? OR email = ?";
+
+    if ($stmt = mysqli_prepare($link, $sql)) {
+      # Bind variables to the statement as parameters
+      mysqli_stmt_bind_param($stmt, "ss", $param_user_login, $param_user_login);
+
+      # Set parameters
+      $param_user_login = $user_login;
+
+      # Execute the statement
+      if (mysqli_stmt_execute($stmt)) {
+        # Store result
+        mysqli_stmt_store_result($stmt);
+
+        # Check if user exists, If yes then verify password
+        if (mysqli_stmt_num_rows($stmt) == 1) {
+          # Bind values in result to variables
+          mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
+
+          if (mysqli_stmt_fetch($stmt)) {
+            # Check if password is correct
+            if (password_verify($user_password, $hashed_password)) {
+
+              # Store data in session variables
+              $_SESSION["id"] = $id;
+              $_SESSION["username"] = $username;
+              $_SESSION["loggedin"] = TRUE;
+
+              # Redirect user to index page
+              echo "<script>" . "window.location.href='./'" . "</script>";
+              exit;
+            } else {
+              # If password is incorrect show an error message
+              $login_err = "The email or password you entered is incorrect.";
+            }
+          }
+        } else {
+          # If user doesn't exists show an error message
+          $login_err = "Invalid username or password.";
+        }
+      } else {
+        echo "<script>" . "alert('Oops! Something went wrong. Please try again later.');" . "</script>";
+        echo "<script>" . "window.location.href='./login.php'" . "</script>";
+        exit;
+      }
+
+      # Close statement
+      mysqli_stmt_close($stmt);
+    }
+  }
+
+  # Close connection
+  mysqli_close($link);
+}
+?>
