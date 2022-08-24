49bc95991658e213140542aede88b98f4bf54118
Ndi23 <bobycruz911@gmail.com>
2022-08-24 13:59:24 +0100

    changed to innitial dbase name ??

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Trivia Ninja Q&A Collection Platform
 The Complement for the Trivia Ninja app by Touko Banix
+
+Project Coordinators:
+
+Mr Tumbu John
+Mr Godlove
+
+Back-End Developers:
+
+Therese Marie
+Ndi Nlomo Raoul
+
+
--- lib/helpers/connection.db.php
+++ lib/helpers/connection.db.php
@@ -3,7 +3,7 @@
 $servername = "localhost"; //Development Server
 $username = "username";
 $password = "password";
-$dbname = "DB";
+$dbname = "Trivia_Ninja_Q_A_Db";
 $port = 3306; //default MySQL Port
 
 // Create database connection --> dbc or just db
@@ -13,9 +13,5 @@ $db = new mysqli($servername, $username, $password, $dbname, $port);
 if ($db->connect_error) {
   die("<b>Connection failed: </b>" . $db->connect_error);
 }
-
-
-
-
 
 ?>
