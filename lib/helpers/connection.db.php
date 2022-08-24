18d7531dbe511edcfa0f7da7994db8ec8138d3f9
Tumbu John <61092841+tumbujohn@users.noreply.github.com>
2022-08-24 06:30:38 +0100

    Update connection.db.php
    DB connection helper

--- lib/helpers/connection.db.php
+++ lib/helpers/connection.db.php
@@ -1,1 +1,16 @@
+//<?php
+//MySQL Class Parameters
+$servername = "localhost"; //Development Server
+$username = "username";
+$password = "password";
+$dbname = "DB";
+$port = 3306; //default MySQL Port
+
+// Create database connection --> dbc or just db
+$db = new mysqli($servername, $username, $password, $dbname, $port);
+
+// Check connection
+if ($db->connect_error) {
+  die("<b>Connection failed: </b>" . $db->connect_error);
+}
 
