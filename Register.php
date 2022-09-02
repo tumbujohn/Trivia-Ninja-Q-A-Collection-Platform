0055776c9b251e548fcf315bf38e0706db1bc463
Ndi23 <bobycruz911@gmail.com>
2022-09-02 13:43:23 +0100

    password hashing added

--- Register.php
+++ Register.php
@@ -13,23 +13,25 @@ if(isset($_POST['signup'])){
   $fname = $_POST['fname'];
   $lname = $_POST['lname'];
   $username = $_POST['username'];
-  // $tel = $_POST['tel'];
+  $tel = $_POST['tel'];
   $email = $_POST['email'];
   $password = $_POST['password'];
    $cpassword = $_POST['cpassword'];
 
   
-  // if ($password !== $cpassword){
-  //   echo "Password do not match";
-  // }
-  // else{
+  if ($password !== $cpassword){
+    echo "Password do not match";
+  }
+  else{
    
-  $sql = "INSERT INTO users (fname, lname, username, email, password) VALUES ('$fname', '$lname','$username','$email','$password')";
+    $pass=md5($password);
+
+  $sql = "INSERT INTO users (fname, lname, username, email,tel, password) VALUES ('$fname', '$lname','$username','$email','$tel','$pass')";
   $send=mysqli_query($db, $sql);
   
   if($send){
     
-  $query = mysqli_query($db, "SELECT * FROM `users` WHERE `username` = '$username' AND `password` = '$password'") or die(mysqli_error());
+  $query = mysqli_query($db, "SELECT * FROM `users` WHERE `username` = '$username' AND `password` = '$pass'") or die(mysqli_error());
   $fetch = mysqli_fetch_assoc($query);
   $row = mysqli_num_rows($query);
 
@@ -45,7 +47,7 @@ if(isset($_POST['signup'])){
     echo "Error:" .$sql . "<br>" . mysql_error($db);
   }
 }
-// }
+}
   
    
    ?>
