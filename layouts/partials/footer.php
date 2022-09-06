01239e7adbaf2b0f1f9ae9f537239f074c64961b
ntomnwi <ntomnwijoshua@gmail.com>
2022-09-06 15:40:28 +0100

    updates on footer

--- layouts/partials/footer.php
+++ layouts/partials/footer.php
@@ -7,16 +7,20 @@
         <div class="row">
             <div class="col logo">
                 <img src="../asset/images/trivia.png" alt="logo">
+                <h3>Trivia Ninja</h3>
                 <p>Subscribe to get latest updates on Trivia Ninja app so you won't miss out or be left behind by what this amazing features the app has to offer to you.
                 </p>
             </div>
             <div class="col location">
+            <img src="../asset/icons/office-icon.png" alt="" class="office" width="80px" >
                 <h3>Our Office</h3>
                 <p>Bonduma Gate, Buea.</p>
                 <p class="email-id">trivianinja@gmail.com</p>
                 <h4 class="number">+237 67******0</h4>
             </div>
+
             <div class="col links">
+                <img src="../asset/icons/link-2.png" alt="" class="link" width="80px">
                 <h3>Links</h3>
                 <ul>
                     <li><a href="">Home</li>
@@ -27,19 +31,6 @@
                 </ul>
             </div>
         </div>
-        <div class="newsletter">
-                <h3>Newsletter</h3>
-                <form class="footer_form">
-                    <i class="fa-solid fa-envelope"></i>
-                    <input type="email" placeholder="Enter your email id"required>
-                    <button type="submit"><i class="fa-solid fa-arrow-right"></i></button>
-                </form>
-                <div class="social-icon">
-                    <i class="fa-brands fa-facebook-f"></i>
-                    <i class="fa-brands fa-instagram"></i>
-                    <i class="fa-brands fa-whatsapp"></i>
-                </div>
-            </div>
     </footer>
 </body>
 </html>
